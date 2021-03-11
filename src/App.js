483bc4cd437cbff8561e50a21f42cca4bc0b712b
bokoness <bokoness@gmail.com>
2021-03-11 23:13:49 +0200

    fixed sidebar styles

--- src/App.js
+++ src/App.js
@@ -13,32 +13,39 @@ import Home from "./component/Home/Home";
 import Sidebar from "./component/Menu";
 
 const App = () => {
-  const [listNote, setListNote] = useState({});
-  return (
-    <>
-      <Title />
+	const [listNote, setListNote] = useState({});
+	return (
+		<>
+			<Title />
 
-      <div className="contener" style={{ display: "flex", direction: "rtl" }}>
-        <div
-          className="Sidebar"
-          style={{
-            background: "burlywood",
-            padding: "20px 0 20px 20px",
-            height: "100%",
-            width: "160px",
-            position: "fixed",
-            zIndex: "1",
-            right: "0",
-            overflowX: "hidden",
-          }}
-        >
-          <Sidebar />
-        </div>
-      </div>
-      <div className="home" style={{ width: "1040px", margin: "5px auto" }}>
-        <Home />
-      </div>
-    </>
-  );
+			<div
+				className="contener"
+				style={{ display: "flex", direction: "rtl" }}
+			>
+				<div
+					className="Sidebar"
+					style={{
+						background: "burlywood",
+						display: "flex",
+						justifyContent: "center",
+						height: "100%",
+						width: "260px",
+						position: "fixed",
+						zIndex: "1",
+						right: "0",
+						overflowX: "hidden",
+					}}
+				>
+					<Sidebar />
+				</div>
+			</div>
+			<div
+				className="home"
+				style={{ width: "1040px", margin: "5px auto" }}
+			>
+				<Home />
+			</div>
+		</>
+	);
 };
 export default App;
--- src/component/Menu.js
+++ src/component/Menu.js
@@ -1,15 +1,15 @@
 import { Menu, Button } from "antd";
 import {
-  AppstoreOutlined,
-  MenuUnfoldOutlined,
-  MenuFoldOutlined,
-  PieChartOutlined,
-  DesktopOutlined,
-  ContainerOutlined,
-  MailOutlined,
-  BulbOutlined,
-  BellOutlined,
-  RestOutlined,
+	AppstoreOutlined,
+	MenuUnfoldOutlined,
+	MenuFoldOutlined,
+	PieChartOutlined,
+	DesktopOutlined,
+	ContainerOutlined,
+	MailOutlined,
+	BulbOutlined,
+	BellOutlined,
+	RestOutlined,
 } from "@ant-design/icons";
 import React, { useState } from "react";
 import Home from "./Home/Home";
@@ -25,67 +25,66 @@ const archive = "/archive";
 const trash = "/trash";
 
 const styleButton = {
-  width: "100%",
-  height: "auto",
-  display: "flex",
-  padding: "20px 0px 20px 20px",
-  margin: "5px 0px 5px 5px",
+	width: "250px",
+	height: "50px",
+	display: "flex",
+	margin: "5px 0px 5px 5px",
 };
 
 const Sidebar = () => {
-  const [contener, setContener] = useState("");
-  const [headers, setHeaders] = useState("הערות");
+	const [contener, setContener] = useState("");
+	const [headers, setHeaders] = useState("הערות");
 
-  const list = [
-    {
-      func: { header: "הערות", contener: <Home /> },
-      icon: <BulbOutlined />,
-      text: "הערות",
-    },
-    {
-      func: { header: "תזכורות", contener: <Reminders /> },
-      icon: <BellOutlined />,
-      text: "תזכורות",
-    },
-    {
-      func: { header: "עריכת תוית", contener: <Home /> },
-      icon: <ContainerOutlined />,
-      text: "עריכת תוית",
-    },
-    {
-      func: { header: "ארכיון", contener: <Archiv /> },
-      icon: <ContainerOutlined />,
-      text: "ארכיון",
-    },
-    {
-      func: { header: "אשפה", contener: <Trash /> },
-      icon: <RestOutlined />,
-      text: "אשפה",
-    },
-  ];
-  const allButton = list.map((button) => (
-    <div
-      key={button.text}
-      onClick={() => {
-        console.log(button.text);
-        setHeaders(button.func.header);
-        setContener(button.func.contener);
-      }}
-    >
-      <div>
-        <Button style={styleButton}>
-          <div>{button.icon}</div>
-          <div>{button.text}</div>
-        </Button>
-      </div>
-    </div>
-  ));
-  return (
-    <>
-      <div className="menu" dir="rtl">
-        {allButton}
-      </div>
-    </>
-  );
+	const list = [
+		{
+			func: { header: "הערות", contener: <Home /> },
+			icon: <BulbOutlined />,
+			text: "הערות",
+		},
+		{
+			func: { header: "תזכורות", contener: <Reminders /> },
+			icon: <BellOutlined />,
+			text: "תזכורות",
+		},
+		{
+			func: { header: "עריכת תוית", contener: <Home /> },
+			icon: <ContainerOutlined />,
+			text: "עריכת תוית",
+		},
+		{
+			func: { header: "ארכיון", contener: <Archiv /> },
+			icon: <ContainerOutlined />,
+			text: "ארכיון",
+		},
+		{
+			func: { header: "אשפה", contener: <Trash /> },
+			icon: <RestOutlined />,
+			text: "אשפה",
+		},
+	];
+	const allButton = list.map(button => (
+		<div
+			key={button.text}
+			onClick={() => {
+				console.log(button.text);
+				setHeaders(button.func.header);
+				setContener(button.func.contener);
+			}}
+		>
+			<div>
+				<Button style={styleButton}>
+					<div>{button.icon}</div>
+					<div>{button.text}</div>
+				</Button>
+			</div>
+		</div>
+	));
+	return (
+		<>
+			<div className="menu" dir="rtl">
+				{allButton}
+			</div>
+		</>
+	);
 };
 export default Sidebar;
